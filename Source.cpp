6289ee479c6c9283235a9b903c9e2ee89344d0ef
Jonathan (Log-Hei) Leung <loghei.leung@ontariotechu.net>
2019-10-27 19:57:36 -0400

    Instruction change
    hopefully it works

--- Source.cpp
+++ Source.cpp
@@ -67,7 +67,7 @@ int instruction()
 	while (1)
 	{
 		char back;
-		cout << "Enter input: Start.\nPlayer 1 and two are prompt in order to input the line corresponding to the row the Player wants to put their piece in.\n";
+		cout << "Goal - To connect four of your player specific pieces in a row.\nPlayer 1 has the X piece, and the 2nd player has the N piece.\n\nRules - Each player takes a turn in placing their pieces in the empty slots on the digital board.\nIn order to win, four of a players pieces must connect either horizontally, vertically or diagonally.\nIf the digital board is all filled up, the players have the option in either restarting or take it as a draw.\n\nMovement - Players select the numbered column where they want to place their piece\n\nCan play against the computer or with a friend.";
 		cout << "Input Back to go back to the main menu.\n";
 		cin >> back;
 		switch (back)
